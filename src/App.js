7fc867716fbc12a878239505dcaecb706e28fa5f
xuebingwei <xuebingwei@bytedance.com>
2022-01-21 20:39:33 +0800

    修改组件文本

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        这是一个测试文本
+        这是一个测试文本  好累呀  Are you tried ???
       </header>
     </div>
   );
